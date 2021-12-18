d774734635a170ff3be0a69b0022d80ab558cdda
Furqon Mauladani <furqonmauladani@gmail.com>
2021-12-18 17:00:59 +0700

    Update app.py

--- app.py
+++ app.py
@@ -19,4 +19,4 @@ def index():
     return render_template('index.html',training=training,lab=lab)
 
 if __name__=="__main__":
-    app.run(debug=True, host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=80)
