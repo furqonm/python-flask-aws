a3dd77b633b9b7f961ab233ac2c5fab0bc850c89
Furqon Mauladani <furqonmauladani@gmail.com>
2022-09-07 22:05:55 +0700

    Update

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ## Python Flask on AWS
 _Aplikasi website menggunakan python dan framework Flask yang dipackage kedalam docker image. Aplikasi dibuild menggunakan service AWS CodeBuild dan testing menggunakan Ghost Inspector. Statusnya bisa dilihat menggunakan badgenya_
-![CodeBuild Status](https://codebuild.us-east-1.amazonaws.com/badges?uuid=eyJlbmNyeXB0ZWREYXRhIjoiRVV2L0FMS2tTR3gyV0lkNjdWSXlDVFV0VTVJOXNDa0I3R0FNYU9QcFlLNVJ2RDdha2lBQlVnOHlkK01Mc0ViYUJBWUVCcDd2cnBUK1dlMkZuYnNpM1hNPSIsIml2UGFyYW1ldGVyU3BlYyI6Ikh3RFJNSldQdHlLd1FBT0IiLCJtYXRlcmlhbFNldFNlcmlhbCI6MX0%3D&branch=main)
-![Ghost Inspector Status](https://api.ghostinspector.com/v1/tests/626421934766b25814df6f2b/status-badge)
+![CodeBuild Status](https://codebuild.us-east-1.amazonaws.com/badges?uuid=eyJlbmNyeXB0ZWREYXRhIjoiNUViN2dlZDJldXM2bHBWdDhYdkdnZkhSKzh1M296TVA1RlZlQVAzZWZpbEozSWc1WWpmNlRZZDVLK1Y1dVlxTmFzSzRjVUhIQlBGNEMzVnFFekw1RE5vPSIsIml2UGFyYW1ldGVyU3BlYyI6InFGL3JwS0JZbWtReEJ3SEsiLCJtYXRlcmlhbFNldFNlcmlhbCI6MX0%3D&branch=main)
+![Ghost Inspector Status](https://api.ghostinspector.com/v1/tests/6316a25acd380e119cd7c9a8/status-badge)
 
 ![Index aplikasi](static/index.png)
--- app.py
+++ app.py
@@ -1,13 +1,10 @@
 """
 Form online untuk pendataan data peserta sebelum kelas AWS dimulai
 """
-
-# Include AWS SDK untuk bahasa Python, serta Flask dan render_template untuk website framework.
 import boto3
 from flask import Flask, render_template
 
 app = Flask(__name__)
-# Menggunakan AWS SDK untuk mengakses service System Manager (SSM) di region tertentu.
 ssm = boto3.client('ssm', region_name="us-east-1")
 
 @app.route('/')
@@ -20,12 +17,10 @@ def index():
     """
     training = ssm.get_parameter(Name='/belajaraws/course', WithDecryption=False)
     training = training['Parameter']['Value']
-# Mengecek apakah nama training adalah CPE, jika ya maka Flash tidak mengenerate informasi lab.
-    lab = True
-    if training == 'Cloud Practitioner Essentials':
-        lab = False
-# Membuat halaman html dari inputan yang diterima agar bisa ditampilkan ke pengguna.
-    return render_template('index.html',training=training,lab=lab)
+    link = ssm.get_parameter(Name='/belajaraws/link', WithDecryption=False)
+    link = link['Parameter']['Value']
+
+    return render_template('index.html',training=training,link=link)
 
 if __name__=="__main__":
     app.run(host='0.0.0.0',port=80)
